f010b05d142816863b474437769190eae24ad861
schroederno <nschroeder@practicevelocity.com>
2017-06-13 13:24:14 -0500

    noah
    init

---
+++ PageViewYahtzee/Models/Player.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PageViewYahtzee.Models
+{
+    public class Player
+    {
+        private int playerNum;
+        private ObservableCollection<int> scores = new ObservableCollection<int>(new List<int>() { -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 0, 0, 0, 0, 0 });
+        private ObservableCollection<bool> scoreable = new ObservableCollection<bool>(new List<bool>() { false, false, false, false, false, false, false, false, false, false, false, false, false, false });
+        public Player(int playerNum)
+        {
+            this.playerNum = playerNum;
+        }
+        
+
+    }
+}
---
+++ PageViewYahtzee/ViewModels/PlayerViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using DevExpress.Mvvm.DataAnnotations;
+using DevExpress.Mvvm;
+
+namespace PageViewYahtzee.ViewModels
+{
+    [POCOViewModel]
+    public class PlayerViewModel
+    {
+    }
+}
